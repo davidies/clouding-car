c067cde98651a56355b7538cbe5888bed3ac9726
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-09-08 22:45:42 -0400

    feat: Add customer namespace in api v1
    CUSTOMER_NS was added in the API_V1

--- src/v1/__init__.py
+++ src/v1/__init__.py
@@ -4,3 +4,9 @@ from flask_restplus import Api
 
 BLUEPRINT_V1 = Blueprint('blueprint_v1', __name__)
 API_V1 = Api(BLUEPRINT_V1, version='1.0', title='Clouding Car', license='MIT')
+
+
+from .controllers import CUSTOMER_NS
+
+
+API_V1.add_namespace(CUSTOMER_NS)
