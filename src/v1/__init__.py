9f4f15e1d70fbe1461b0e062e5879ca8466e3b98
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-09-09 14:22:50 -0400

    feat: Add Brand namespace in API_V1

--- src/v1/__init__.py
+++ src/v1/__init__.py
@@ -6,7 +6,8 @@ BLUEPRINT_V1 = Blueprint('blueprint_v1', __name__)
 API_V1 = Api(BLUEPRINT_V1, version='1.0', title='Clouding Car', license='MIT')
 
 
-from .controllers import CUSTOMER_NS
+from .controllers import BRAND_NS, CUSTOMER_NS
 
 
+API_V1.add_namespace(BRAND_NS)
 API_V1.add_namespace(CUSTOMER_NS)
