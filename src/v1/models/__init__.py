3c3c153554882620c2e088c78bd99a981d457f14
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-09-08 23:08:57 -0400

    feat: Implement Brand model

--- src/v1/models/__init__.py
+++ src/v1/models/__init__.py
@@ -2,6 +2,17 @@ from flask_restplus import fields
 from .. import API_V1
 
 
+class Brand:
+    __model__ = API_V1.model('Brand', {
+        'id': fields.Integer(readonly=True, min=1, description='The brand id'),
+        'name': fields.String(required=True, description='The brand name')
+    })
+
+    def __init__(self, identifier: int, name: str, /) -> None:
+        self.id = identifier
+        self.name = name
+
+
 class Customer:
     __model__ = API_V1.model('Customer', {
         'id': fields.Integer(readonly=True, min=1, description='The customer id'),
