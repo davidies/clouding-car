56ad3d83b76899268fe7ed713f59d63030001e74
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-09-11 18:52:47 -0400

    fix: Check the existence of SECRET_KEY
    The existence of SECRET_KEY was not check, so it bug was fixed and now a exception is raised if it is not present as a environment variable

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 from .v1 import BLUEPRINT_V1
 
 APP = Flask(__name__)
-APP.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+if 'SECRET_KEY' in os.environ:
+    APP.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+else:
+    raise KeyError("'SECRET_KEY' was not set as enviroment variable")
 APP.register_blueprint(BLUEPRINT_V1, url_prefix='/api/v1', strict_slashes=False)
