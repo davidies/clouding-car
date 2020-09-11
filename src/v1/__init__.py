857e8fb60751a6e763872141d887e78383cbc177
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-09-10 22:13:06 -0400

    feat: Add AUTH_NS to API_V1

--- src/v1/__init__.py
+++ src/v1/__init__.py
@@ -6,9 +6,10 @@ BLUEPRINT_V1 = Blueprint('blueprint_v1', __name__)
 API_V1 = Api(BLUEPRINT_V1, version='1.0', title='Clouding Car', license='MIT')
 
 
-from .controllers import BRAND_NS, CAR_NS, CUSTOMER_NS
+from .controllers import AUTH_NS, BRAND_NS, CAR_NS, CUSTOMER_NS
 
 
+API_V1.add_namespace(AUTH_NS)
 API_V1.add_namespace(BRAND_NS)
 API_V1.add_namespace(CAR_NS)
 API_V1.add_namespace(CUSTOMER_NS)
