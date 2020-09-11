682b723381db23dd23a579ec9eee0395890bf39d
David Ignacio Espinoza Saavedra <david.espinoza.saavedra@gmail.com>
2020-09-10 22:00:02 -0400

    feat: Add constant messages in constants.py

--- src/v1/shared/constants.py
+++ src/v1/shared/constants.py
@@ -1,5 +1,9 @@
 # Messages
+BEARER_TOKEN_REQUIRED = 'Bearer token required'
+INVALID_CREDENTIALS = 'Invalid username or password'
+INVALID_REFRESH_TOKEN = 'Invalid refresh token'
 NOT_FOUND = 'Not Found'
+SUCCESSFUL_LOGIN = 'Successful login'
 SUCCESSFULLY_ADDED = 'Successfully added'
 SUCCESSFULLY_DELETED = 'Successfully deleted'
 SUCCESSFULLY_UPDATED = 'Successfully updated'
